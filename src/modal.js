caac72f65f3455acea3a1a75486d48045a0ed8fd
AdamczukPawel <104322297+AdamczukPawel@users.noreply.github.com>
2022-07-26 15:13:33 +0200

    Update modal

--- src/modal.js
+++ src/modal.js
@@ -1,13 +1,11 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
+    openModalBtn: document.querySelectorAll('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.forEach(el => el.addEventListener('click', toggleModal));
   refs.closeModalBtn.addEventListener('click', toggleModal);
-
   function toggleModal() {
     refs.modal.classList.toggle('backdrop--hidden');
   }
